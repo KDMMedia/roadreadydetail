c91a7d4e87c71c3e05268a260e7c3293659e3dca
KDMMedia <140282287+KDMMedia@users.noreply.github.com>
2023-07-23 00:04:47 +0200

    Add files via upload

---
+++ docs/test.php
@@ -0,0 +1,33 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] ?? '' == "POST") {
+    $name = $_POST["name"] ?? '';
+    $email = $_POST["email"] ?? '';
+    $message = $_POST["message"] ?? '';
+
+    // Email details
+    $to = "kyanodemaertelaere@gmail.com"; // Replace with your email address
+    $subject = "New Form Submission";
+    $body = "Name: $name\nEmail: $email\nMessage: $message";
+    $headers = "From: $email";
+
+    // Send email
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Email sent successfully!";
+    } else {
+        echo "Failed to send email.";
+    }
+}
+?>
+
+<form action="send_email.php" method="post">
+    <label for="name">Name:</label>
+    <input type="text" id="name" name="name" required>
+
+    <label for="email">Email:</label>
+    <input type="email" id="email" name="email" required>
+
+    <label for="message">Message:</label>
+    <textarea id="message" name="message" required></textarea>
+
+    <input type="submit" value="Send">
+</form>
